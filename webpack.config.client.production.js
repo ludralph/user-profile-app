8d6002e48eb7ff851572e018b5128a2e752e71f0
Raphael Etim <raphaelumoh@gmail.com>
2020-07-08 12:11:24 +0100

    add webpack config files

---
+++ webpack.config.client.production.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const webpack = require("webpack");
+const CURRENT_WORKING_DIRECTORY = process.cwd();
+
+const config = {
+    mode: "production",
+    devtool: "eval-source-map",
+    entry: [
+      path.join(CURRENT_WORKING_DIRECTORY, 'client/main.js')
+    ],
+    output: {
+      path: path.join(CURRENT_WORKING_DIRECTORY, '/dist'),
+      filename: 'bundle.js',
+      publicPath: '/dist/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader'
+          ]
+        }
+      ]
+  
+    },
+  
+  }
+
+module.exports = config;
---
+++ webpack.config.server.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+const CURRENT_WORKING_DIRECTORY = process.cwd();
+
+const config = {
+  name: "server",
+  entry: [path.join(CURRENT_WORKING_DIRECTORY, './server/server.js')],
+  target: "node",
+  output: {
+    path: path.join(CURRENT_WORKING_DIRECTORY, '/dist'),
+    filename: 'server.generated.js',
+    publicPath: '/dist/',
+    libraryTarget: "commonjs2"
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+
+  }
+
+}
+module.exports = config;
